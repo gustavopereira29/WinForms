94666b6ed5308b286ebc3f73dff53262770b4e7d
Gustavo Pereira <gustavoh2915@gmail.com>
2022-11-05 00:09:01 -0300

    criacao de banco

---
+++ source/repos/WinForms/Solucao02/WinApp02/Modelo/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinApp02.Modelo
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Salario { get; set; }
+        public string Sexo { get; set; }
+        public string TipoContrato { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAtualizacao { get; set; }
+    }
+}
